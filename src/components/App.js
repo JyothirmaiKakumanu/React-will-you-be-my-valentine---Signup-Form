aee2e4fb15a45d3e0850d10a04e84b25ac0ba36d
Jyothirmai <jyothirmai.kakumanu2711@gmail.com>
2021-04-22 16:16:14 +0530

    committed

--- src/components/App.js
+++ src/components/App.js
@@ -1,93 +1,96 @@
-import React, { Component, useState } from "react";
-import "../styles/App.css";
-import { useForm } from "react-hook-form";
+import React, {useState} from "react";
+import '../styles/App.css';
 
 const App = () => {
-  const { register, handleSubmit } = useForm();
-  const [msg, setMsg] = useState("");
-  const isAlphaNumeric = (str) => {
-    var val = str;
-    var RegEx = /^[a-z0-9]+$/i;
-    var Valid = RegEx.test(val);
-    return Valid;
-  };
-  const onSubmit = (data) => {
-    //console.log(data.gender)
-    if (
-      data.name === "" ||
-      data.email === "" ||
-      data.gender === "" ||
-      data.number === "" ||
-      data.password === ""
-    ) {
-      setMsg("All fields are mandatory");
-      return;
-    }
-    //console.log(isAlphaNumeric(data.name))
-    if (isAlphaNumeric(data.name) === false) {
-      setMsg("Name is not alphanumeric");
-      return;
-    }
-    if (data.email.indexOf("@") === -1) {
-      setMsg("Email must contain @");
-      return;
-    }
-    if (isNaN(data.number)) {
-      setMsg("Phone Number must contain only numbers");
-      return;
-    }
-    if (data.password.length < 6) {
-      setMsg("Password must contain atleast 6 letters");
-      return;
-    }
-    const d = data.email.indexOf("@");
 
-    setMsg(`Hello ${data.email.substring(0, d)}`);
-    return;
-  };
+  const [name,setName] = useState('')
+  const [email,setEmail] = useState('')
+  const [gender,setGender] = useState('Male')
+  const [phone,setPhone] = useState('')
+  const [password,setPassword] = useState('')
+  const [errorMsg,seterrorMsg] = useState('')
+  
+  const UpdateName = (e)=>{
+    setName(e.target.value)
+  }
+
+  const UpdateEmail=(e)=>{
+    setEmail(e.target.value)
+  }
+
+  const UpdateGender=(e)=>{
+    setGender(e.target.value)
+  }
+
+  const UpdatePhone=(e)=>{
+    setPhone(e.target.value);
+  }
+
+  const UpdatePassword=(e)=>{
+    setPassword(e.target.value)
+  }
+
+  const ValidateForm=(e)=>{
+    if(!name || !email || !gender || !phone || !password){
+      e.preventDefault();
+      seterrorMsg("All fields are mandatory");
+      return;
+    }
+    else if(!name.match(/^[0-9a-z]+$/)){
+      e.preventDefault();
+      seterrorMsg("Name is not alphanumeric");
+      return;
+    }
+    else if(!email.match(/\S+@\S+\.\S+/)){
+      e.preventDefault();
+      seterrorMsg("Email must contain @")
+      return;
+    }
+    else if(!phone.match(/^\d{10}$/)){
+      e.preventDefault();
+      seterrorMsg("Phone Number must contain only numbers");
+      return;
+    }
+    else if(password.length<6){
+      e.preventDefault();
+      seterrorMsg("Password must contain atleast 6 letters");
+      return;
+    }
+    else{
+      e.preventDefault();
+      var displayName = email.substring(0, email.lastIndexOf("@"));
+      seterrorMsg("Hello "+displayName);
+    }
+  }
+
 
   return (
-    <div id="main">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          name="name"
-          data-testid="name"
-          ref={register}
-          placeholder="Name"
-        />
-        <br></br>
-        <input
-          name="email"
-          data-testid="email"
-          ref={register}
-          placeholder="email"
-        />
-        <br></br>
-        <select name="gender" data-testid="gender" ref={register}>
-          <br></br>
-          <option>Male</option>
-          <option>Female</option>
-          <option>Others</option>
-        </select>
-        <input
-          name="number"
-          data-testid="phoneNumber"
-          ref={register}
-          placeholder="Phone"
-        />
-        <br></br>
-        <input
-          type="password"
-          name="password"
-          data-testid="password"
-          ref={register}
-          placeholder="Password"
-        />
-        <br></br>
-        <input type="submit" data-testid="submit" />
-        <p>{msg}</p>
-      </form>
-    </div>
+    <div className='container' >
+            <h1>Sign-up Form</h1>
+          <form onSubmit={ValidateForm} >
+            <label htmlFor="name">Name:</label>
+            <input type="text" data-testid ='name'  onChange={UpdateName} />
+            <br/>
+            <label htmlFor="email">Email address:</label>
+            <input type="email" data-testid = 'email' onChange={UpdateEmail} />
+            <br/>
+            <label htmlFor="gender">Gender:</label>
+            <select name="gender" data-testid = 'gender' value={gender}  onChange={UpdateGender} >
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Others">Others</option>
+            </select>
+            <br/>
+            <label htmlFor="phone">Phone Number:</label>
+            <input type="tel"  data-testid = 'phoneNumber' onChange={UpdatePhone} />
+            <br/>
+            <label htmlFor="password">Password:</label>
+            <input type="password" data-testid = 'password' onChange={UpdatePassword} />
+            <br/>
+            <button  data-testid = 'submit'>Submit</button>
+            <p>{errorMsg}</p>
+          </form>
+        </div>
   );
 };
 
